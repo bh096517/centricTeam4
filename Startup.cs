e8f7a975e0b6bdbb42e795b19c3ff6d443823948
Hentosz <bh096517@ohio.edu>
2021-03-03 08:47:54 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(centricTeam4.Startup))]
+namespace centricTeam4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
