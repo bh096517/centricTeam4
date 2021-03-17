7250f246b166f4606b52f1082502b4f1732ebc69
Breanna Hentosz <bh096517@ohio.edu>
2021-03-17 15:30:21 -0400

    Rebuilding profile

---
+++ DAL/MIS4200Team4Context.cs
@@ -0,0 +1,21 @@
+﻿using centricTeam4.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+    namespace centricTeam4.DAL
+    {
+        public class MIS4200Team4Context : DbContext
+        {
+            public MIS4200Team4Context() : base("name=DefaultConnection")
+            {
+
+            }
+            public DbSet<Profile> profile { get; set; }
+
+
+        }
+    }
+  
---
+++ Models/Profile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace centricTeam4.Models
+{
+    public class Profile
+    {
+        public Guid ProfileID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Display(Name = "Full Name")]
+        public string fullName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+        }
+        [Required]
+        [Display(Name = "Office")]
+        public string office { get; set; }
+        [Required]
+        [Display(Name = "Current Position")]
+        public string position { get; set; }
+
+        [Display(Name = "Hire Date")]
+        //[DisplayFormat(DataFormatString = DateTime, ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
+        [Display(Name = "Primary Phone")]
+        [Phone]
+        public string phoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        public string photo { get; set; }
+
+    }
+}
