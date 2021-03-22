300da53dcecf3dab0186671b7b150a9812184dac
Breanna Hentosz <bh096517@ohio.edu>
2021-03-22 14:37:02 -0400

    updated database

--- Models/recognition.cs
+++ Models/recognition.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using System.Windows.Controls;
 
 namespace centricTeam4.Models
 {
     public class recognition
     {
+        
         public int recognitionID { get; set; }
         public string description { get; set; }
         public DateTime orderDate { get; set; }
